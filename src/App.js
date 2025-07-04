7c2bcc840195ae802ab8947dc0406c3a69e9167e
AnshD109 <ansh.dankhara@ue-germany.de>
2025-07-04 12:30:18 +0200

    fixing

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <html>
+        <body>
+          <h1>Ansh Dankhara</h1>
+        </body>
+      </html>
+    </div>
+  );
+}
