dd5f5153f2aed2c45505bf0cf899ce6e6b5bbee5
AnshD109 <ansh.dankhara@ue-germany.de>
2025-07-05 16:45:51 +0200

    home page set

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+  import { useRef } from 'react';
+  import Lightning from './components/Lightning';
+  import VariableProximity from './components/VariableProximity';
+  import './App.css'; 
 
-function App() {
-  return (
-    <div>
-      <html>
-        <body>
-          <h1>Ansh Dankhara</h1>
-        </body>
-      </html>
-    </div>
-  );
-}
+  function App() {
+    // --- MOVE THIS LINE HERE ---
+    const containerRef = useRef(null);
 
-export default App;
+    return (
+      <div className="App">
+        <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1, pointerEvents: 'none' }}>
+          <Lightning />
+        </div>
+
+        {/* This ref is now correctly defined before being used */}
+        <div
+          ref={containerRef}
+          style={{position: 'relative'}}
+        >
+          
+          <VariableProximity
+            label={'Aspiring Data Scientist | Passionate About Machine Learning & AI '}
+            className={'variable-proximity-demo'}
+            fromFontVariationSettings="'wght' 400, 'opsz' 9"
+            toFontVariationSettings="'wght' 100, 'opsz' 40"
+            containerRef={containerRef}
+            radius={100}
+            falloff='linear'
+          />
+        </div>
+        
+      </div>
+    );
+  }
+
+  export default App;
